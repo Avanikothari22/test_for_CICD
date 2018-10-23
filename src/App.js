cecdce7ffdfba3520777ea5bd92314ff60309218
Avani <avanikothari2223@gmail.com>
2018-10-23 12:39:51 +0530

    added export default.

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { store  } from './store';
 import Home from './Home';
-export default class App extends Component{ 
-  render(){
+ const App = ()=>{ 
   <Provider store={store}>
     <Home />
   </Provider>
-  }
-}
+ }
+export default App;
 
 
 
